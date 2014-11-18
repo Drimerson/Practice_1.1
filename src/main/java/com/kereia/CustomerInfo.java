a549ee883a9de226938a349269946bc0b3d52b67
KObzar <wwiinn@ukr.net>
2014-11-18 22:43:35 +0200

    commit

---
+++ src/main/java/com/kereia/CustomerInfo.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.kereia;
+
+import java.io.Serializable;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Администратор
+ */
+@Named(value = "customerInfo")
+@Stateless
+public class CustomerInfo implements Serializable {
+
+    @EJB
+    private NewClass nc;
+    
+    /**
+     * Creates a new instance of CustomerInfo
+     */
+    public CustomerInfo() {
+    }
+    
+    public String info() {
+        return nc.getString();
+    }
+    
+}
---
+++ src/main/java/com/kereia/NewClass.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.kereia;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Администратор
+ */
+@Stateless
+public class NewClass {
+    
+    
+    
+    public String getString() {
+        return "LALA";
+    }
+    
+}
